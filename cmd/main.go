4d7b71172546a70f0cccbd28bee7e86fde5f4e5d
Shruti Patel <shruti222patel@gmail.com>
2020-01-01 11:51:45 -0500

    Add leetcode problem scrapping

--- cmd/main.go
+++ cmd/main.go
@@ -7,22 +7,21 @@ import (
 	"os"
 	"strings"
 	"text/template"
-	"time"
 
-	"github.com/PuerkitoBio/goquery"
 	"github.com/chromedp/cdproto/cdp"
 	"github.com/chromedp/chromedp"
 	"github.com/chromedp/chromedp/device"
 )
 
 type LeetCodeProblem struct {
-	Name          string
-	Number        string
-	Description   string
-	Example       string
-	Difficulty    string
-	Url           string
-	RelatedTopics string
+	Name            string
+	Number          string
+	Description     string
+	Example         string
+	Difficulty      string
+	Url             string
+	RelatedTopics   string
+	RelatedProblems string
 }
 
 func main() {
@@ -54,10 +53,6 @@ func generateTemplates() {
 	err = t.ExecuteTemplate(os.Stdout, "main_test.tmpl", lc)
 }
 
-func toSlug(str string) string {
-	return strings.ReplaceAll(strings.ToLower(str), " ", "-")
-}
-
 func getLeetCodeProblem(url string) *LeetCodeProblem {
 	lc := LeetCodeProblem{
 		Name:          "Test",
@@ -72,66 +67,113 @@ func getLeetCodeProblem(url string) *LeetCodeProblem {
 }
 
 type LeetCodeScrapper struct {
-	URL      string
-	document *goquery.Document
+	URL string
+	ctx context.Context
 }
 
 func NewLeetCodeScrapper(url string) *LeetCodeScrapper {
 	lcs := LeetCodeScrapper{
 		URL: url,
 	}
-	// lcs.getHTML()
+
 	return &lcs
-}
-
-func (lcs *LeetCodeScrapper) getHTML() {
-
 }
 
 func (lcs *LeetCodeScrapper) ScrapeData() (*LeetCodeProblem, error) {
 	lcp := LeetCodeProblem{}
-	fmt.Printf("%+v", lcs.document)
 
-	opts := append(chromedp.DefaultExecAllocatorOptions[:]) // chromedp.Flag("headless", false),
+	opts := append(chromedp.DefaultExecAllocatorOptions[:], chromedp.Flag("headless", false))
 	// chromedp.Flag("disable-gpu", false),
 	// chromedp.Flag("enable-automation", false),
 	// chromedp.Flag("disable-extensions", false),
 
-	allocCtx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
-	defer cancel()
+	allocCtx, cancelAlloc := chromedp.NewExecAllocator(context.Background(), opts...)
+	defer cancelAlloc()
 
 	// create context
-	ctx, cancel := chromedp.NewContext(allocCtx, chromedp.WithLogf(log.Printf))
-	defer cancel()
+	ctx, cancelCtx := chromedp.NewContext(allocCtx, chromedp.WithLogf(log.Printf))
+	defer cancelCtx()
+
+	lcs.ctx = ctx
 
 	if err := chromedp.Run(ctx,
-		chromedp.Emulate(device.IPhone7landscape),
+		chromedp.Emulate(device.IPadlandscape),
 		chromedp.Navigate(lcs.URL),
-		chromedp.Sleep(2000*time.Millisecond),
-
-		// chromedp.WaitVisible(`#thing`),
+		chromedp.WaitVisible(`/html/body/div[1]/div`),
 	); err != nil {
 		return nil, fmt.Errorf("Couldn't run chrome browser")
 	}
 
 	// Find name & number
-	var nameNode []*cdp.Node
-	if err := chromedp.Run(ctx,
-		chromedp.Nodes("//h3/text()", &nameNode),
-	); err != nil {
-		return nil, fmt.Errorf("Couldn't find")
+	nameNodes, err := lcs.getNode("/html/body/div[1]/div/div[2]/div/div/div[1]/div/div[1]/div[1]/div/div[2]/div/div[1]/div[1]/text()")
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't find name node")
 	}
-	// Name
-	lcp.Name = nameNode[2].Dump("", "", false)
-	fmt.Println(lcp.Name)
+	lcp.Number = lcs.cleanScrapedString(nameNodes[0].Dump("", "", false))
+	lcp.Name = lcs.cleanScrapedString(nameNodes[2].Dump("", "", false))
 
-	// Number
-	lcp.Number = nameNode[0].Dump("", "", false)
-	fmt.Println(lcp.Number)
+	// Find description
+	descNodes, _ := lcs.getNode("/html/body/div[1]/div/div[2]/div/div/div[1]/div/div[1]/div[1]/div/div[2]/div/div[2]/div/p/text()")
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't find descp node")
+	}
+	lcp.Description = lcs.cleanScrapedString(lcs.concatNodeStr(descNodes))
 
-	fmt.Println("Document tree:")
-	fmt.Print(nameNode[0].Value)
+	// Find explaination
+	exampleNodes, err := lcs.getNode("/html/body/div[1]/div/div[2]/div/div/div[1]/div/div[1]/div[1]/div/div[2]/div/div[2]/div/pre/text()")
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't find example node")
+	}
+	lcp.Example = lcs.cleanScrapedString(exampleNodes[0].Dump("", "", false))
+
+	// Find related topics
+	relatedTopicsNodes, err := lcs.getNode("/html/body/div[1]/div/div[2]/div/div/div[1]/div/div[1]/div[1]/div/div[2]/div/div[6]/div[2]/a/span/text()")
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't find example node")
+	}
+	lcp.RelatedTopics = lcs.cleanScrapedString(lcs.concatNodeStr(relatedTopicsNodes))
+
+	// Find difficulty
+	difficultyNodes, err := lcs.getNode("/html/body/div[1]/div/div[2]/div/div/div[1]/div/div[1]/div[1]/div/div[2]/div/div[1]/div[2]/div/text()")
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't find related topics node")
+	}
+	lcp.Difficulty = lcs.cleanScrapedString(difficultyNodes[0].Dump("", "", false))
+
+	// Find related problems
+	relatedProblemsNodes, err := lcs.getNode("/html/body/div[1]/div/div[2]/div/div/div[1]/div/div[1]/div[1]/div/div[2]/div/div[7]/div[2]/div/a/text()")
+	if err != nil {
+		return nil, fmt.Errorf("Couldn't find related problems node")
+	}
+	// TODO loop over all related problems
+	lcp.RelatedProblems = lcs.cleanScrapedString(lcs.concatNodeStr(relatedProblemsNodes))
+
 	fmt.Println("Finished printing nodes")
 
 	return &lcp, nil
+}
+
+func (lcs *LeetCodeScrapper) getNode(fullXPath string) ([]*cdp.Node, error) {
+	var node []*cdp.Node
+	err := chromedp.Run(lcs.ctx,
+		chromedp.Nodes(fullXPath, &node),
+	)
+	return node, err
+}
+
+func (lcs *LeetCodeScrapper) concatNodeStr(nodes []*cdp.Node) string {
+	str := ""
+	for _, n := range nodes {
+		str += n.Dump(" ", "", false)
+	}
+	return str
+}
+
+func (lcs *LeetCodeScrapper) cleanScrapedString(str string) string {
+	cleanedStr := strings.ReplaceAll(str, "#text ", "")
+	return strings.ReplaceAll(cleanedStr, `"`, "")
+}
+
+func toSlug(str string) string {
+	return strings.ReplaceAll(strings.ToLower(str), " ", "-")
 }
